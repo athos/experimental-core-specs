fd81e17537fbfedf3970fd87df43483f38d6d807
OHTA Shogo <athos0220@gmail.com>
2017-10-19 09:34:25 +0900

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to experimental-core-specs
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/experimental_core_specs/core_test.clj
@@ -0,0 +1,7 @@
+(ns experimental-core-specs.core-test
+  (:require [clojure.test :refer :all]
+            [experimental-core-specs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
