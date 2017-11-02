fb88daddfab35c521c9fe1dff225ae34ea9493c7
OHTA Shogo <athos0220@gmail.com>
2017-11-02 16:29:14 +0900

    Bump CLJ

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta2"]])
+  :dependencies [[org.clojure/clojure "1.9.0-beta4"]]
+  :profiles {:dev {:dependencies [[pinpointer "0.1.0-SNAPSHOT"]]}})
